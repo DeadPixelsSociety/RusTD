247c4f5c552ec4ab3b49549ebbd820f1abaff11e
Vizepi <joseph.kieffer@edu.univ-fcomte.fr>
2015-10-07 14:42:37 +0200

    Added framerate limit

--- src/lib/main.cpp
+++ src/lib/main.cpp
@@ -59,7 +59,9 @@ int main()
     lev->addTower(t3);
     lev->addTower(t4);
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "RusTD");
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "RusTD");
+    window.setFramerateLimit(60);
+    resizeWindow(window, 1600, 900);
     sf::Clock clockwerk;
 
     while(window.isOpen())
